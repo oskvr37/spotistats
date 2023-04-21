fa2883d15296da60c6e03723ae671b098748f85a
oskvr37 <oskarxpd@gmail.com>
2023-04-21 16:25:40 +0200

    finished user auth

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -1,9 +1,44 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+// response contains state and code / error
+
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
-  const { code, state } = Object.fromEntries(searchParams);
+  const { state, code, error } = Object.fromEntries(searchParams);
 
-  return NextResponse.json({ code, state });
+  // TODO: handle errors
+  // if error is `access_denied` then redirect
+  // to login page with error message
+
+  if (error) {
+    return NextResponse.json({ error });
+  }
+
+  // app should compare the state parameter that it received in
+  // the redirection URI with the state parameter it originally
+  // provided to Spotify in the authorization URI
+
+  // the state is generated at api/login and passed to Spotify
+  // and then is returned from Spotify to api/callback
+  // we need to store initial state somewhere to compare it with
+  // the state returned from Spotify
+
+  const request_state = request.cookies.get("AUTH_STATE")?.value;
+
+  if (!request_state) {
+    return NextResponse.json({ error: "invalid_cookies" });
+  }
+
+  if (state !== request_state) {
+    return NextResponse.json({ error: "state_mismatch" });
+  }
+
+  // successful authentication
+
+  // If the user accepted your request, then your app is ready to
+  // exchange the authorization code for an Access Token.
+  // It can do this by making a POST request to the /api/token endpoint.
+
+  return NextResponse.json({ code });
 }
--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,20 +1,32 @@
-import { redirect } from 'next/navigation';
+import { NextResponse } from "next/server";
 
-const CLIENT_ID = process.env.CLIENT_ID
-const REDIRECT_URI = process.env.REDIRECT_URI
-
-const SCOPE = 'user-read-private user-read-email'
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const SCOPE = "user-read-private user-read-email";
 
 function generateRandomString() {
-  var text = ''
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 16; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return text
+  return text;
 }
 
 export function GET() {
-    redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&state=${generateRandomString()}`)
+  const state = generateRandomString();
+  const redirect_uri = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&state=${state}`;
+
+  // store state in cookies and then redirect to Spotify auth
+
+  const headers = new Headers();
+
+  headers.set(
+    "Set-Cookie",
+    `AUTH_STATE=${state}; Path=/; HttpOnly; Secure; SameSite=Strict`
+  );
+
+  return NextResponse.redirect(redirect_uri, { headers });
 }
