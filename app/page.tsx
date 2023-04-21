1178828e756739d3b11f95367d7c937e1f920b03
oskvr37 <oskarxpd@gmail.com>
2023-04-21 17:12:12 +0200

    add login button

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <main>
       <h1>Spotify Stats</h1>
+      <button>
+        <a href="http://localhost:3000/api/login">Login</a>
+      </button>
     </main>
   )
 }
